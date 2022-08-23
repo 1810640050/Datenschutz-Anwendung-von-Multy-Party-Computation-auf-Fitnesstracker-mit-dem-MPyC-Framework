90b498a6ab9cf30098c10e14c3c91328b838f2e9
martino1988 <martin.andreas@gmx.at>
2022-08-23 23:17:15 +0200

    GPS Shares berechnen

--- python_files/create_gps_shares.py
+++ python_files/create_gps_shares.py
@@ -7,10 +7,28 @@ import json
 import pickle
 from dateutil.parser import parse
 
-secflt = mpc.SecFxp()
+secfxp = mpc.SecFxp()
+secflt = mpc.SecFlt()
+
+
 
 async def main():
     await mpc.start()
+
+    """
+    https://stackoverflow.com/questions/8040388/convert-lat-lon-to-integer
+    https://stackoverflow.com/questions/2579535/convert-dd-decimal-degrees-to-dms-degrees-minutes-seconds-in-python
+    https://en.proft.me/2015/09/20/converting-latitude-and-longitude-decimal-values-p/
+    
+    
+    double originalLat = 38.898748, TEMPdecimal;
+    int degrees = (int)originalLat;
+    TEMPdecimal = (originalLat - degrees) * 60;
+    int minutes = (int)TEMPdecimal;
+    TEMPdecimal = (TEMPdecimal - minutes) * 60;
+    int seconds = (int)TEMPdecimal;
+    int lat = (degrees * 10000) + (minutes * 100) + seconds;
+    """
 
     list = rwcsv.get_CSV_as_List("../"+defs.PATH_FOR_GPS_INPUTFILES + defs.NAME_OF_GPS_INPUTFILE)
     list.pop(0)
@@ -27,8 +45,9 @@ async def main():
 
 
         sec_lat = mpc.input(secflt(latitude))[0]
-        sec_lon = mpc.input(secflt(longitude))[0]
-
+        sec_lon = mpc.input(secfxp(longitude))[0]
+        a = await mpc.output(sec_lat)
+        aa = await mpc.output(sec_lon)
         field_of_lat = await mpc.gather(sec_lat)
         field_of_lon = await mpc.gather(sec_lon)
 
